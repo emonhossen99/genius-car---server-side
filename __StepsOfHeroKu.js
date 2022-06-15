35ac8aea82ff76c419293a6e7ada05ac6731aaec
emonhossen99 <mdxhamedemon@gmail.com>
2022-06-15 21:44:59 +0600

    added new change

---
+++ __StepsOfHeroKu.js
@@ -0,0 +1,30 @@
+/*
+*----------------------
+* ONE TIME FOR YOUR COMPUTER
+*--------------------------
+* 1.Create heroku account 
+* 2.Verify email 
+* 3.Install heroku cli
+* 4.Heroku Login
+*---------------------
+* FOR EACH PROJECT ONE TIME
+*---------------------------
+* 1.heroku create
+* 2.make sure git add . git commit .git push
+* 3.git push heroku main
+* 4.go to Heroku Dashboard > Current Project > Reveal Config vars
+* 5.Copy paste config vars from your .env file
+* 6.Make sure you have whitelisted all ip to access mongodb
+*--------------------
+* UPDATE Server with new change
+*--------------------------
+* 1. Make change
+* 2.make sure git add . git commit .git push
+* 3.git push heroku main
+*---------------------
+*Connect Server to client 
+*---------------------
+* 1.replace localhost by heroku link
+* 2.npm run build
+* 3.firebase deploy
+*/
